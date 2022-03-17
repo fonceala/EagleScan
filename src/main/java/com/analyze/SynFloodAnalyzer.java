a509e17dea510feed520e4bda0c4236ba8e2212d
fonceala <tam1u18@soton.ac.uk>
2022-03-17 19:30:12 +0200

    added functionality for detecting the number of occurencies of a dstaddrs

--- src/main/java/com/analyze/SynFloodAnalyzer.java
+++ src/main/java/com/analyze/SynFloodAnalyzer.java
@@ -10,6 +10,7 @@ public class SynFloodAnalyzer {
     private Map<InetAddress,Integer> numOfAppearances;
 
     private int duration;
+    private final int EXPECTED_DURATION = 2000;
     private final int PERCENTAGE = 80;
 
     public SynFloodAnalyzer(Map<String,InetAddress> synFloodMap, int duration){
@@ -22,14 +23,24 @@ public class SynFloodAnalyzer {
         boolean isFlood = false;
 
         for(String srcAddr: synFloodMap.keySet()){
+
             InetAddress dstAddr = synFloodMap.get(srcAddr);
+
             boolean exists = false;
             for(InetAddress addr: numOfAppearances.keySet()){
                 if(addr.toString().equals(dstAddr.toString())){
-
+                    exists = true;
                 }
             }
+            if(!exists){
+                numOfAppearances.put(dstAddr,1);
+            }else{
+                numOfAppearances.replace(dstAddr,numOfAppearances.get(dstAddr)+1);
+            }
+
         }
+
+
 
         return isFlood;
     }
